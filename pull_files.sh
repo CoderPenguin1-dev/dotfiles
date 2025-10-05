fbf39b7fc958dd85dc05ff63d69dd16dec5167fe
CoderPenguin1-dev <coderpenguin1.dev.gmail.com>
2025-10-05 16:54:21 -0400

    Update pull_files and gitignore to remove xfce configs

--- pull_files.sh
+++ pull_files.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
+
 # Bashrc
 cp ~/.bashrc .bashrc
 cp ~/.aliases .aliases
 
 # Application Configs
 cp ~/.config/kitty/kitty.conf config/kitty/kitty.conf
-cp -r ~/.config/xfce4 config/xfce4
 cp /etc/pacman.conf etc/pacman.conf

7eb3c7d4af4f7b095d3adff97495283b5b7483bb
CoderPenguin1-dev <coderpenguin1.dev.gmail.com>
2025-10-05 17:13:37 -0400

    Add some XFCE configs, add btop config

--- pull_files.sh
+++ pull_files.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# XFCE Configs
+cp -r ~/.config/xfce4/xfconf/xfce-perchannel-xml config/xfce4/xfconf/
+
 # Bashrc
 cp ~/.bashrc .bashrc
 cp ~/.aliases .aliases
 
 # Application Configs
 cp ~/.config/kitty/kitty.conf config/kitty/kitty.conf
+cp ~/.config/btop/btop.conf config/btop/btop.conf
 cp /etc/pacman.conf etc/pacman.conf

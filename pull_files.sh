063b5f8a5d84a2d36167ce4bab65b0cbde4c3213
CoderPenguin1-dev <coderpenguin1.dev@gmail.com>
2025-10-08 02:14:34 -0400

    Add environment variables, resorted pull_files.sh, and add sled config

--- pull_files.sh
+++ pull_files.sh
@@ -10,4 +10,8 @@ cp ~/.aliases .aliases
 # Application Configs
 cp ~/.config/kitty/kitty.conf config/kitty/kitty.conf
 cp ~/.config/btop/btop.conf config/btop/btop.conf
+cp ~/.config/sled.conf config/sled.conf
+
+# Other System Configs
 cp /etc/pacman.conf etc/pacman.conf
+cp /etc/environment etc/environment
